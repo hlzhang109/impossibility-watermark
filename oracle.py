20a5ac14f5131be00b269f506a74da0c55afe2ae
borito1907 <boranerol03@gmail.com>
2023-12-20 22:14:33 +0300

    Modified quality oracle

--- oracle.py
+++ oracle.py
@@ -207,7 +207,7 @@ class Oracle:
     
     @property
     def instruction(self):
-        return f"Below are two candidate responses to the query {self.query}: "
+        return f"Below are two candidate responses to the query {self.query}:\n "
         # f"Below are two candidate completions to the news article prefix ``{self.query}'': "
 
     @property
@@ -239,6 +239,9 @@ class Oracle:
     	(4) Response B is a little better than response A
     	(5) Response B is much better than response A
         '''
+    
+    def get_score_dict(self):
+        return {1: 1, 2: 0.5, 3: 0, 4: -0.5, 5:-1} if self.choice_granularity == 5 else {1: 1, 2: 0, 3: -1} 
 
     @property
     def three_choice(self):
@@ -251,10 +254,24 @@ class Oracle:
     @property
     def answer_prompt(self):
         choices = self.five_choice if self.choice_granularity == 5 else self.three_choice
-        return f'''So based on your reasoning, choose one of the following {choices}''' # So which of the two responses is a better response to the given prompt? 
+        return f'''So based on your reasoning, choose one of the following {choices}'''
     
-    def query_gpt_once(self, prompt, model="gpt-3.5", max_tokens=5, tokenizer=None):
-        # avoid using max_tokens which can be too short for generating explanations.
+    def query_gpt_once(self, paraphrased_response, model="gpt-3.5", max_tokens=5, tokenizer=None, invert_order=False):
+        """
+        Args:
+        paraphrased_response (str): the original and the paraphrased response
+        """
+        if invert_order:
+            response_1 = paraphrased_response
+            response_2 = self.response
+        else:
+            response_1 = self.response
+            response_2 = paraphrased_response
+
+        prompt = self.instruction + f"Response 1: {response_1}\n" + f"Response 2: {response_2}"
+        print(f"Prompt: {prompt}")
+        print(f"Model: {model}")
+        # Avoid using max_tokens which can be too short for generating explanations.
         n_attempt = 0
         while n_attempt < 5:
             try:
@@ -265,8 +282,8 @@ class Oracle:
                 oracle_answer = chat(self.answer_prompt, history=history, model=model, tokenizer=tokenizer)
                 pattern = r'\((\d+)\)'
                 filtered_response = re.findall(pattern, oracle_answer)
-                print(oracle_reasoning)
-                print(oracle_answer)
+                print(f"Oracle Reasoning: {oracle_reasoning}")
+                print(f"Oracle Answer: {oracle_answer}")
                 score = int(filtered_response[-1])
                 if score not in range(1, self.choice_granularity+1):
                     print(f"return choice {score} not in [1, {self.choice_granularity}]")
@@ -299,44 +316,75 @@ class Oracle:
             return 3
        
     def maintain_quality(self, paraphrased_response, tie_threshold=0.1, model="gpt-3.5", max_tokens=5, tokenizer=None):
+        """
+        Use both the reward model and GPT to see if the paraphrased response maintains the quality.
+        """
+        # First round of comparison
         choice = self.query_rm_once(paraphrased_response, self.response, tie_threshold=tie_threshold)
-        self.choice_granularity = 3
-        # positive score means paraphrased response wins
-        score_dict = {1: 1, 2: 0.5, 3: 0, 4: -0.5, 5:-1} if self.choice_granularity == 5 else {1: 1, 2: 0, 3: -1} 
+        score_dict = self.get_score_dict()
         if choice is None:
             return False
         score = score_dict[choice]
-        print()
-        print("Second round of comparison:")
-        print()
+        # Secound round of comparison
         second_choice = self.query_rm_once(self.response, paraphrased_response, tie_threshold=tie_threshold)
         if second_choice is None:
             return False
-        
+        # We subtract now because the positions are reversed.
         score -= score_dict[second_choice]
         if score < 0:
             return False
-        elif score >=0 and self.check_quality:
-            prompt = f"Original response: {self.response}" + "\n" + "New response: " + paraphrased_response + "\n" + self.check_quality_prompt #self.check_error_prompt
+        if self.check_quality:
+            prompt = f"Original response: {self.response}" + "\n" + "New response: " + paraphrased_response + "\n" + self.check_quality_prompt
+            # They had a check_error_prompt that they later commented out.
             check_quality = chat(prompt, model=model, tokenizer=tokenizer)
-            if 'yes' in check_quality.lower(): # new response is at least as good as the original response
-                return True
-            else:
-                return False 
-        else:
-            return True
+            return 'yes' in check_quality.lower() # new response is at least as good as the original response
+        return True
 
     def report_mean_score(self, paraphrased_response, tie_threshold=0.1, model="gpt-3.5", max_tokens=5, tokenizer=None):
+        """
+        Compare the paraphrased response and the original response using GPT.
+        To account for GPT's position bias, swap their position and report the mean.
+        Positive scores indicate that the paraphrased response is better.
+        """
+        # First round of comparison
         choice = self.query_gpt_once(paraphrased_response, self.response)
-        self.choice_granularity = 3
-        # NOTE positive scores for paraphrased response winning
-        score_dict = {1: 1, 2: 0.5, 3: 0, 4: -0.5, 5:-1} if self.choice_granularity == 5 else {1: 1, 2: 0, 3: -1} 
+        score_dict = self.get_score_dict()
         if choice is None:
             return False
         score = score_dict[choice]
-        print()
-        print("Second round of comparison:")
-        print()
+        # Second round of comparison
         choice = self.query_gpt_once(self.response, paraphrased_response, tie_threshold=tie_threshold)
+        if choice is None:
+            return False
         second_score = score_dict[choice]
-        return (score-second_score)/2 # essentially (1, 0), (1, -1), (1, 1), (0, 1), (0, -1), (0, 0), (-1, 1), (-1, 0), (-1, -1) -> 0.5, 0, 1, 0.5, -0.5, 0, 0, -0.5, -1
+
+        # We subtract the second score since the positions are now inverted.
+        return (score-second_score)/2
+    
+if __name__ == '__main__':
+
+    query = "Write me a good story."
+
+    response ="""
+    Once upon a time in a mystical forest, there lived a young girl named Elara, who had the unique ability to communicate with animals. Elara's best friend was a wise old owl named Hoot, who had seen many seasons pass in the forest.
+    One day, the tranquility of the forest was disturbed by a strange rumbling sound. Elara and Hoot discovered that a giant machine, driven by people from the city, was cutting down the trees. The forest creatures were in panic, and their home was in danger.
+    Determined to save the forest, Elara decided to seek the help of the legendary Green Dragon, known to be the guardian of nature. Despite being warned of the dragon's fierce nature, Elara and Hoot ventured deep into the unexplored parts of the forest.
+    After days of journeying, they finally found the Green Dragon in a hidden valley. The dragon was initially distrustful, but Elara's genuine concern for the forest and her ability to speak with animals convinced the dragon of her sincerity.
+    The Green Dragon agreed to help and revealed an ancient secret to Elara – a magical song that could awaken the spirits of the forest. Elara, with the help of Hoot and the forest animals, sang the magical song under the full moon.
+    Miraculously, the spirits of the forest awoke. The trees began to move, gently at first, then with purpose. They formed a barrier, halting the progress of the machines. The people from the city, witnessing this extraordinary event, realized the importance of the forest and the error of their ways.
+    From that day on, the forest was protected, and the animals lived in peace. Elara became known as the Guardian of the Forest, and the Green Dragon, once feared, was celebrated as its protector. Elara and Hoot continued to watch over the forest, ensuring its safety and harmony for many years to come.
+    And so, the forest remained a magical place, where the spirits danced in the moonlight, and the voice of a young girl who spoke for the trees echoed in the wind, reminding all of the delicate balance between humans and nature.
+    """
+
+    paraphrased_response = """
+    In a far away coastal nook, stood a lighthouse, protected through ages. An aging keeper, Eli, kept watch, - while a lovely bird, Edward, would light the darkened seas. This beautiful heroic abode, stood firm for when fog had blown. 
+    Eli held his lone head high, stayed, - steady, - while Edward set their lantern, alight - - and then lifted their flag, high, - which sent a beacon of light, towards their beloved home.
+    ""The local villagers, admired them - and while the keeper, - a man named Eli, stood vigilant - the entire lighthouse, owed its life to his helpmate - a bird, named Edward. - - and they became the symbol, for NorthStar.
+    """
+
+    oracle = Oracle(query, response, check_quality=True, choice_granularity=5, use_chat_arena_prompt= True)
+
+    
+    response = oracle.query_gpt_once(paraphrased_response)
+
+    print(response)

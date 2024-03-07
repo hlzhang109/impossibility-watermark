58be685c2c744d9b3be46b656eb529b26c7dfa85
Fabrice Harel-Canada <fabricehc@pluslab-a6000.cs.ucla.edu>
2024-03-06 19:02:05 -0800

    adding evan study results

--- stationary_distribution_analysis.sh
+++ stationary_distribution_analysis.sh
@@ -11,13 +11,17 @@ CondaEnv=$CONDA_DEFAULT_ENV # The name of the current conda environment to activ
 cuda=0  # Initial cuda value - This initialization needs to be before the loop starts
 
 for ((i=1; i<=N; i++)); do
+
+    # Wait a few seconds so that the results files are saved separately
+    sleep 10
+
     # Create a new tmux session for each command run, detached (-d) and named ("session_$i")
     tmux new-session -d -s "session_$i"
     
     # Use tmux send-keys to activate the conda environment and then run the command
     # Ensure we correctly reference the variables inside the send-keys command
     tmux send-keys -t "session_$i" "conda activate $CondaEnv" C-m
-    tmux send-keys -t "session_$i" "python -m attack attack_args.cuda=\'$cuda\' &> 4_evan_2_${i}.txt" C-m
+    tmux send-keys -t "session_$i" "python -m attack attack_args.cuda=\'$cuda\' attack_args.save_name=./eval/results/4_evan_2_${i}.csv &> 4_evan_2_${i}.txt" C-m
 
     # Increment cuda for the next iteration, and ensure it loops through the gpus
     ((cuda++))

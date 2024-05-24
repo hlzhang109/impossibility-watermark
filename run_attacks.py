2e9b5a008df0c6ed46e05614abcaeb81241e2dd3
boranerol03@gmail.com <borito1907@pluslab01-a6000.cs.ucla.edu>
2024-05-24 01:14:47 -0700

    trying to attack semstamp, doesn't work right now

---
+++ run_attacks.py
@@ -0,0 +1,50 @@
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+log = logging.getLogger(__name__)
+
+def run_command(command, filepath):
+    log.info(f"Running command: {command}")
+    log.info(f"Saving results to {filepath}")
+
+    try:
+        with open(filepath, "w") as f:
+            # Redirect both stdout and stderr to the same file
+            result = subprocess.run(command, shell=True, check=True, text=True, stdout=f, stderr=subprocess.STDOUT)
+        log.info("Command executed successfully")
+        return "Success", None
+    except subprocess.CalledProcessError as e:
+        log.error(f"Command failed with error: {e.stderr}")
+        # Returning None, stderr if there was an error
+        return None, e.stderr
+
+def main():
+    results = []
+
+    for prompt_num in range(1,2):
+
+        log_filepath = f"./logs/semstamp_attacks/minic4_{prompt_num}_first.log"
+        
+        command = f"python attack.py " \
+                  f"++attack_args.prompt_file='./inputs/mini_c4.csv' " \
+                  f"++attack_args.prompt_num={prompt_num} " \
+                  f"++attack_args.watermarked_text_path='./inputs/c4_{prompt_num}.csv' " \
+                  f"++attack_args.watermarked_text_num=1 " \
+                  f"++attack_args.results_dir='./semstamp_attacks/c4/' " \
+                  f"++watermark_args.save_file_name='c4_{prompt_num}.csv' " \
+                  f"++watermark_args.only_detect=True " \
+                  f"++attack_args.is_completion=True " 
+
+        stdout, stderr = run_command(command, log_filepath)
+        if stderr is None:
+            print(f"Command succeeded: {command}\nOutput:\n{stdout}")
+        else:
+            print(f"Command failed: {command}\nError:\n{stderr}")
+        results.append((stdout, stderr))
+
+    log.info(results)
+
+if __name__ == "__main__":
+    main()

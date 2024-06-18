8d37c2b32b08a2c50c0ed5d2df8a9c4814809702
Boran Erol boranerol@ucla.edu <borito1907@pluslab-a6000.cs.ucla.edu>
2024-06-18 10:54:46 -0700

    prepared quality analysis script for the new generatinos

--- run_gen_quality_analysis.py
+++ run_gen_quality_analysis.py
@@ -1,5 +1,5 @@
 from oracles.absolute import PrometheusAbsoluteOracle
-from utils import get_prompt_or_output
+from utils import get_prompt_or_output, replace_multiple_commas
 
 def main():
     oracle = PrometheusAbsoluteOracle()
@@ -9,32 +9,25 @@ def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
     log = logging.getLogger(__name__)
 
-    prompt_nums = [1, 2, 3]
+    prompt_nums = [3,6,7,11]
 
-    # prompt_based_or_c4 = "prompt_based_saves" 
-    prompt_based_or_c4 = "c4_saves"
+    base_folder_name = "new_prompt_based_saves" # "c4_saves" "prompt_based_saves"
 
-    # TODO: This is pretty dumb. We wanna have the same settings for both.
-    if prompt_based_or_c4 == "prompt_based_saves":
-        temps = [1, 1.3, 1.7]
-        divps = [5, 15, 20]
-    else:
-        temps = [0.5, 1, 1.5, 1.8]
-        divps = [0,15,20]
+    temps = [0.5, 1, 1.5, 1.8]
+    divps = [0,10,20]
 
 
     for attempt in range(1, 4):
         for temp in temps:
             for divp in divps:
                 for prompt_num in prompt_nums:
-                    if prompt_based_or_c4 == "prompt_based_saves":
+                    if "prompt_based_saves" in base_folder_name:
                         folder_name = f'prompt_{prompt_num}_temp_{int(temp * 100)}_divp_{divp}_attempt_{attempt}'
                     else:
                         folder_name = f'c4_{prompt_num}_temp_{int(temp * 100)}_divp_{divp}_attempt_{attempt}'
-                    input_folder_name = f'./inputs/{prompt_based_or_c4}/{folder_name}'
+                    input_folder_name = f'./inputs/{base_folder_name}/{folder_name}'
 
-                    # NOTE: Only works with prompt_based right now.
-                    if prompt_based_or_c4 == "prompt_based_saves":
+                    if "prompt_based_saves" in base_folder_name:
                         prompt_path = './inputs/tests_v1_with_lotr.csv'
                         prompt = get_prompt_or_output(prompt_path, prompt_num)
                     else:
@@ -44,14 +37,14 @@ def main():
 
                     log.info(f"Prompt\n {prompt}")
 
-                    watermarked_text_path = f"{input_folder_name}/watermarked_text_without_commas.csv"
+                    watermarked_text_path = f"{input_folder_name}/watermarked_text.csv"
                     watermarked_text_num = 1
 
                     watermarked_text = get_prompt_or_output(watermarked_text_path, watermarked_text_num)
 
                     log.info(f"Watermarked Text\n {watermarked_text}")
 
-                    log_filepath = f"./inputs/{prompt_based_or_c4}/{folder_name}/quality_logfile_without_commas.log"
+                    log_filepath = f"./inputs/{base_folder_name}/{folder_name}/quality_logfile.log"
                     log.info(f"Running Absolute Oracle with {folder_name}")
 									    
                     try:

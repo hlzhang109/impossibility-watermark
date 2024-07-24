95420543696a27f4423528988de8bdac893e7775
hlzhang109 <hlzhang109@gmail.com>
2024-07-24 14:04:26 -0400

    update readme

--- text/watermark/xsir/__init__.py
+++
@@ -1,6 +0,0 @@
-# ====================================================
-# __init__.py
-# Description: Initialization file for the xsir package
-# ====================================================
-
-from .xsir import XSIR
